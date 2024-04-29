fcf3b8b2c12a1e095cc11501c3b7ca44be227303
Sridhar <sridharmoturu@gmail.com>
2024-04-29 16:36:21 +0530

    almost done

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
-import { data } from './aboutData.js'
-import express from 'express'
-import cors from 'cors'
-import fs from 'fs'
+import { data } from "./aboutData.js";
+import express from "express";
+import cors from "cors";
+import fs from "fs";
 
-const app = express()
+const app = express();
 
-
-
-app.use('/static', express.static('public'))
+app.use(express.json());
+app.use("/static", express.static("public"));
 app.use(
   cors({
-    origin: "https://createnary-client.vercel.app"
     // origin: "http://localhost:5173",
+    origin: "https://createnary-client.vercel.app",
   })
 );
-app.get('/', (req,res) => {
-  res.send('<h1>Welcome to Server</h1>')
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to Server</h1>");
+});
 
-app.get('/api/about', (req,res) => {
-    res.status(200).json(data)
-})
+app.get("/api/about", (req, res) => {
+  res.status(200).json(data);
+});
 app.get("/api/faq", (req, res) => {
   fs.readFile("Faq.json", "utf-8", (err, data) => {
     if (err) {
@@ -35,9 +34,17 @@ app.get("/api/faq", (req, res) => {
   });
 });
 
-const PORT = 4000;
-const start =() => {
-    app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`))
-}
+app.post("/api/calculate", (req, res) => {
+  const { followers, products } = req.body;
+  let followersIncome = followers / 10;
+  let productsIncome = Math.floor(products / 2);
+  const totalIncome = (followersIncome + productsIncome) * 1000;
+  res.status(200).json(totalIncome);
+});
 
-start()
+const PORT = 4000;
+const start = () => {
+  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+};
+
+start();
