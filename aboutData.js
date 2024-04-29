389e7b715df51f54d29e641aa81abca48cb81cd8
Sridhar <sridharmoturu@gmail.com>
2024-04-29 09:55:36 +0530

    initial commit

---
+++ aboutData.js
@@ -0,0 +1,31 @@
+export const data = [
+  {
+    id: 1,
+    title:
+      "Share the holidays, stays and experiences with your friends and family",
+    subTitle:
+      "You receive trending & curated Itineraries and Insider deals on Hotels & Flights to share",
+    image: "/static/image5.png",
+  },
+  {
+    id: 2,
+    title: "Understand their requirements and budgets",
+    subTitle:
+      "You receive trending & curated Itineraries and Insider deals on Hotels & Flights to share",
+    image: "/static/image6.png",
+  },
+  {
+    id: 3,
+    title: "Share the payment link, sit back and relax",
+    subTitle:
+      "You receive trending & curated Itineraries and Insider deals on Hotels & Flights to share",
+    image: "/static/image7.png",
+  },
+  {
+    id: 4,
+    title: "Get Rewarded each time",
+    subTitle:
+      "You receive trending & curated Itineraries and Insider deals on Hotels & Flights to share",
+    image: "/static/image8.png",
+  }
+];
---
+++ app.js
@@ -0,0 +1,36 @@
+import { data } from './aboutData.js'
+import express from 'express'
+import cors from 'cors'
+import fs from 'fs'
+
+const app = express()
+
+
+
+app.use('/static', express.static('public'))
+app.use(cors({
+    origin:'http://localhost:5173'
+}))
+app.get('/api/about', (req,res) => {
+    res.status(200).json(data)
+})
+app.get("/api/faq", (req, res) => {
+  fs.readFile("faq.json", "utf-8", (err, data) => {
+    if (err) {
+      console.error("Error reading FAQ file:", err);
+      return res
+        .status(500)
+        .send("An error occurred while processing your request.");
+    }
+
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).send(data);
+  });
+});
+
+const PORT = 4000;
+const start =() => {
+    app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`))
+}
+
+start()
